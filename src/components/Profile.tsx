a747e7b95f6be6da964eae11eb1a34355dccb43f
tonny0831 <tonnyjansen0831@gmail.com>
2022-10-19 14:00:12 +0300

    .

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,51 +5,51 @@ import Navbar from "./Navbar";
 interface ProfileProps {}
 
 const Profile: FunctionComponent<ProfileProps> = () => {
-  const [userProfile, setUserProfile] = useState<string>("");
+  const [userProfile, setUserProfile] = useState<any>({
+    id: "",
+    name: "",
+    email: "",
+    biz: false,
+  });
   useEffect(() => {
     getUser()
       .then((result) => {
         setUserProfile(result.data);
-        console.log(userProfile);
       })
       .catch((err) => {
-        console.log(err);
+        alert("Something Went Wrong...");
       });
   }, []);
   return (
     <>
       <Navbar />
       <h1 className="text-center mt-5 mb-4">
-        <i className="fa-solid fa-user"></i> Profile Card{" "}
+        <i className="fa-solid fa-user mb-4"></i> Profile
       </h1>
       <div className="user-card">
         <div className="containerDiv">
-          <img src="banner.jpg" alt="cookies" className="hero-image" />
+          <img
+            src="banner.jpg"
+            alt="cookies"
+            className="hero-image img-fluid"
+          />
 
           <div className="information">
-            <img
-              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/avatar.png"
-              alt="avatar"
-              className="avatar"
-            />
-            <div className="name">Name Goes Here</div>
-            <div className="position">Creative Manager</div>
-
-            <div className="stats">
-              <span className="followers">
-                <span className="value">323</span>
-                <span className="label">followers</span>
-              </span>
-
-              <span className="following">
-                <span className="value">290</span>
-                <span className="label">following</span>
-              </span>
-
-              <span className="stories">
-                <span className="value">22</span>
-                <span className="label">stories</span>
-              </span>
+            <img src="avatar.png" alt="avatar" className="avatar" />
+            <span className="mb-4">Currently Signed in As:</span>
+            <div className="name mb-3 ">{userProfile.name}</div>
+            <hr />
+            <div className="mb-2">
+              <strong>Email: </strong>
+              {userProfile.email}
+            </div>
+            <div className="mb-2">
+              <strong>Account Type: </strong>
+              {userProfile.biz ? (
+                <span style={{ color: "green" }}>Business Account </span>
+              ) : (
+                <span>Not Business Account </span>
+              )}
             </div>
           </div>
         </div>
